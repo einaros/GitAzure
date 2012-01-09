3495c84131e6f60e1d95e970d0dad1bd03761e15
einaros <einaros@gmail.com>
2012-01-09 22:51:13 +0100

    process updates

--- bin/gitazure.js
+++ bin/gitazure.js
@@ -8,9 +8,7 @@ var readline = require('readline')
 
 function copyFiles(src, dst, cb) {
   var xcopy = spawn('xcopy', ['/s', src, dst]);
-  xcopy.stdout.on('data', function (data) {
-    console.log(data.toString('utf8'));
-  });
+  xcopy.stdout.on('data', function (data) {});
   xcopy.stderr.on('data', function (data) {
     console.log(data.toString('utf8'));
   });
@@ -31,8 +29,19 @@ function fileExists(path) {
   console.error('Please run GitAzure from within a node.js web role');
   process.exit(-1);
 }*/
+var depsFolder = __dirname + '\\..\\deps\\';
+copyFiles(depsFolder + '7z\\7za.exe', 'bin\\', function(code) {
+  if (!code) {
+    console.error('File copy failed. Please verify that the current folder has a "bin" subfolder, which is writable.');
+    process.exit(-1);
+    return;
+  }
+  copyFiles(depsFolder + 'scripts\\*', 'bin\\', function(code) {
+    if (!code) {
+      console.error('File copy failed. Please verify that the current folder has a "bin" subfolder, which is writable.');
+      process.exit(-1);
+      return;
+    }
 
-copyFiles(__dirname + '/../deps/7z/7za.exe', '.', function(code) {
-  console.log(code);
+  });
 });
-
