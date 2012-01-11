636905527e4cf40da1bac99d951a2a778e33cd23
einaros <einaros@gmail.com>
2012-01-11 16:09:28 +0100

    added basic config init

--- bin/configinit.js
+++ bin/configinit.js
@@ -2,5 +2,9 @@ var fs = require('fs');
 
 var config = {
 	branch: 'master',
-	
+	repoUrl: '',
+	npmUpdate: true,
+	npmPrune: true
 }
+
+fs.writeFileSync('gitazure.json', JSON.stringify(config, null, 2));
