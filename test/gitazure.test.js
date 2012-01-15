10414dfb22e7818bfe539d7ed2e0684028cdb04e
einaros <einaros@gmail.com>
2012-01-15 17:33:35 +0100

    test cleanup

--- test/gitazure.test.js
+++ test/gitazure.test.js
@@ -14,165 +14,197 @@ describe('GitAzure', function() {
     fs.utimesSync = originalUtimesSync;
   }) 
 
-  describe('#listen', function() {
-    it('leaves other requests alone', function(done) {
-      var srv = util.makeServer(++port, function() {
-        GitAzure.listen(srv, 'https://github.com/einaros/Test');
-        util.request(port, '/foo', function (data) {
-          data.should.eql('/foo');
-          srv.close();
-          done();
-        })
-      });
+  it('leaves other requests alone', function(done) {
+    var srv = util.makeServer(++port, function() {
+      GitAzure.listen(srv, 'https://github.com/einaros/Test');
+      util.request(port, '/foo', function (data) {
+        data.should.eql('/foo');
+        srv.close();
+        done();
+      })
     });
+  });
 
-    it('processes requests to hook url, without any parameters', function(done) {
-      var srv = util.makeServer(++port, function() {
-        var hook = GitAzure.listen(srv);
-        var payload = {
-          repository: {
-            url: 'https://github.com/einaros/Test'
-          },
-          ref: 'refs/heads/master'
-        }
-        var called = false;
-        hook.processPayload = function(payload, res) { called = true; res.end(); }
-        util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
-          srv.close();
-          called.should.be.ok;
-          done();
-        })
-      });
+  it('processes requests to hook url, without any parameters', function(done) {
+    var srv = util.makeServer(++port, function() {
+      var hook = GitAzure.listen(srv);
+      var payload = {
+        repository: {
+          url: 'https://github.com/einaros/Test'
+        },
+        ref: 'refs/heads/master'
+      }
+      var called = false;
+      hook.processPayload = function(payload, res) { called = true; res.end(); }
+      util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
+        srv.close();
+        called.should.be.ok;
+        done();
+      })
     });
+  });
 
-    it('processes requests to hook url, matching the repository url and custom branch', function(done) {
-      var srv = util.makeServer(++port, function() {
-        var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure' });
-        var payload = {
-          repository: {
-            url: 'https://github.com/einaros/Test'
-          },
-          ref: 'refs/heads/azure'
-        }
-        var called = false;
-        hook.processPayload = function(payload, res) { called = true; res.end(); }
-        util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
-          srv.close();
-          called.should.be.ok;
-          done();
-        })
-      });
+  it('processes requests to hook url, matching the repository url and custom branch', function(done) {
+    var srv = util.makeServer(++port, function() {
+      var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure' });
+      var payload = {
+        repository: {
+          url: 'https://github.com/einaros/Test'
+        },
+        ref: 'refs/heads/azure'
+      }
+      var called = false;
+      hook.processPayload = function(payload, res) { called = true; res.end(); }
+      util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
+        srv.close();
+        called.should.be.ok;
+        done();
+      })
     });
+  });
 
-    it('does not process requests to hook url, matching the repository url but missing custom branch', function(done) {
-      var srv = util.makeServer(++port, function() {
-        var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure' });
-        var payload = {
-          repository: {
-            url: 'https://github.com/einaros/Test'
-          },
-          ref: 'refs/heads/master'
-        }
-        var called = false;
-        hook.processPayload = function(payload, res) { called = true; res.end(); }
-        util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
-          srv.close();
-          called.should.not.be.ok;
-          done();
-        })
-      });
+  it('does not process requests to hook url, matching the repository url but missing custom branch', function(done) {
+    var srv = util.makeServer(++port, function() {
+      var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure' });
+      var payload = {
+        repository: {
+          url: 'https://github.com/einaros/Test'
+        },
+        ref: 'refs/heads/master'
+      }
+      var called = false;
+      hook.processPayload = function(payload, res) { called = true; res.end(); }
+      util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
+        srv.close();
+        called.should.not.be.ok;
+        done();
+      })
     });
+  });
 
-    it('does not process requests to hook url, matching the branch but missing respository url', function(done) {
-      var srv = util.makeServer(++port, function() {
-        var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure' });
-        var payload = {
-          repository: {
-            url: 'https://github.com/einaros/Test2'
-          },
-          ref: 'refs/heads/azure'
-        }
-        var called = false;
-        hook.processPayload = function(payload, res) { called = true; res.end(); }
-        util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
-          srv.close();
-          called.should.not.be.ok;
-          done();
-        })
-      });
+  it('does not process requests to hook url, matching the branch but missing respository url', function(done) {
+    var srv = util.makeServer(++port, function() {
+      var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure' });
+      var payload = {
+        repository: {
+          url: 'https://github.com/einaros/Test2'
+        },
+        ref: 'refs/heads/azure'
+      }
+      var called = false;
+      hook.processPayload = function(payload, res) { called = true; res.end(); }
+      util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
+        srv.close();
+        called.should.not.be.ok;
+        done();
+      })
     });
+  });
 
-    it('executes git and no npm for simple update', function(done) {
-      var srv = util.makeServer(++port, function() {
-        var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure' });
-        var payload = {
-          repository: {
-            url: 'https://github.com/einaros/Test'
-          },
-          ref: 'refs/heads/azure',
-          commits: [
-            { modified: [ 'foo' ] }, 
-            { modified: [ 'bar' ] }, 
-          ]
-        }
+  it('executes git and no npm for simple update', function(done) {
+    var srv = util.makeServer(++port, function() {
+      var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure' });
+      var payload = {
+        repository: {
+          url: 'https://github.com/einaros/Test'
+        },
+        ref: 'refs/heads/azure',
+        commits: [
+          { modified: [ 'foo' ] }, 
+          { modified: [ 'bar' ] }, 
+        ]
+      }
 
-        var order = [];
-        child_proc.spawn = function(command, args) {
-          args.unshift(command);
-          order.push(args);
-          var proc = util.fakeSpawn();
-          process.nextTick(function() { proc.emit('exit', 0); });
-          return proc;
-        }
-        fs.utimesSync = function(path) { }
+      var order = [];
+      child_proc.spawn = function(command, args) {
+        args.unshift(command);
+        order.push(args);
+        var proc = util.fakeSpawn();
+        process.nextTick(function() { proc.emit('exit', 0); });
+        return proc;
+      }
+      fs.utimesSync = function(path) { }
 
-        util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
-          order.length.should.eql(2);
-          order[0].should.eql(['git', 'fetch', 'origin']);
-          order[1].should.eql(['git', 'reset', '--hard', 'origin/azure']);
-          srv.close();
-          done();
-        })
-      });
+      util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
+        order.length.should.eql(2);
+        order[0].should.eql(['git', 'fetch', 'origin']);
+        order[1].should.eql(['git', 'reset', '--hard', 'origin/azure']);
+        srv.close();
+        done();
+      })
     });
+  });
 
-    it('executes git, npm install and npm prune for changeset which updates package.json', function(done) {
-      var srv = util.makeServer(++port, function() {
-        var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure' });
-        var payload = {
-          repository: {
-            url: 'https://github.com/einaros/Test'
-          },
-          ref: 'refs/heads/azure',
-          commits: [
-            { modified: [ 'foo' ] }, 
-            { modified: [ 'package.json' ] }, 
-          ]
-        }
+  it('executes git, npm install and npm prune for changeset which updates package.json', function(done) {
+    var srv = util.makeServer(++port, function() {
+      var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure' });
+      var payload = {
+        repository: {
+          url: 'https://github.com/einaros/Test'
+        },
+        ref: 'refs/heads/azure',
+        commits: [
+          { modified: [ 'foo' ] }, 
+          { modified: [ 'package.json' ] }, 
+        ]
+      }
 
-        var order = [];
-        child_proc.spawn = function(command, args) {
-          args.unshift(command);
-          order.push(args);
-          var proc = util.fakeSpawn();
-          process.nextTick(function() { proc.emit('exit', 0); });
-          return proc;
-        }
-        fs.utimesSync = function(path) { }
+      var order = [];
+      child_proc.spawn = function(command, args) {
+        args.unshift(command);
+        order.push(args);
+        var proc = util.fakeSpawn();
+        process.nextTick(function() { proc.emit('exit', 0); });
+        return proc;
+      }
+      fs.utimesSync = function(path) { }
 
-        util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
-          order.length.should.eql(4);
-          order[0].should.eql(['git', 'fetch', 'origin']);
-          order[1].should.eql(['git', 'reset', '--hard', 'origin/azure']);
-          order[2].should.eql(['bin\\npm.cmd', 'install']);
-          order[3].should.eql(['bin\\npm.cmd', 'prune']);
-          srv.close();
-          done();
-        })
-      });
+      util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
+        order.length.should.eql(4);
+        order[0].should.eql(['git', 'fetch', 'origin']);
+        order[1].should.eql(['git', 'reset', '--hard', 'origin/azure']);
+        order[2].should.eql(['bin\\npm.cmd', 'install']);
+        order[3].should.eql(['bin\\npm.cmd', 'prune']);
+        srv.close();
+        done();
+      })
     });
+  });
 
-    it('executes git and npm install but no npm prune for changeset which updates package.json, with custom config', function(done) {
+  it('touches gitazure\'s server.js for gitazure.json update', function(done) {
+    var srv = util.makeServer(++port, function() {
+      var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'foobar' });
+      var payload = {
+        repository: {
+          url: 'https://github.com/einaros/Test'
+        },
+        ref: 'refs/heads/foobar',
+        commits: [
+          { modified: [ 'gitazure.json' ] } 
+        ]
+      }
+
+      child_proc.spawn = function(command, args) {
+        var proc = util.fakeSpawn();
+        process.nextTick(function() { proc.emit('exit', 0); });
+        return proc;
+      }
+      var utimesOrder = [];
+      fs.utimesSync = function(path) {
+        utimesOrder.push(path);
+      }
+
+      util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
+        utimesOrder.length.should.eql(2);
+        utimesOrder[1].should.match(/GitAzure\/lib\/\.\.\/server.js$/);
+        srv.close();
+        done();
+      })
+    });
+  });
+
+  describe('custom config', function() {
+    it('executes git and npm install but no npm prune for changeset which updates package.json, when npmPrune is false', function(done) {
       var srv = util.makeServer(++port, function() {
         var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'azure', npmPrune: false });
         var payload = {
@@ -207,7 +239,7 @@ describe('GitAzure', function() {
       });
     });
 
-    it('executes git, but no npm install nor npm prune for changeset which updates package.json, with custom config', function(done) {
+    it('executes git, but no npm install nor npm prune for changeset which updates package.json, when npmInstall is false', function(done) {
       var srv = util.makeServer(++port, function() {
         var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'master', npmInstall: false });
         var payload = {
@@ -239,39 +271,6 @@ describe('GitAzure', function() {
           done();
         })
       });
-    });
-
-    it('touches gitazure\'s server.js for gitazure.json update', function(done) {
-      var srv = util.makeServer(++port, function() {
-        var hook = GitAzure.listen(srv, { repoUrl: 'https://github.com/einaros/Test', branch: 'foobar' });
-        var payload = {
-          repository: {
-            url: 'https://github.com/einaros/Test'
-          },
-          ref: 'refs/heads/foobar',
-          commits: [
-            { modified: [ 'gitazure.json' ] } 
-          ]
-        }
-
-        child_proc.spawn = function(command, args) {
-          var proc = util.fakeSpawn();
-          process.nextTick(function() { proc.emit('exit', 0); });
-          return proc;
-        }
-        var utimesOrder = [];
-        fs.utimesSync = function(path) {
-          utimesOrder.push(path);
-        }
-
-        util.request(port, '/githook', 'payload=' + encodeURIComponent(JSON.stringify(payload)), function (data) {
-          utimesOrder.length.should.eql(2);
-          utimesOrder[1].should.match(/GitAzure\/lib\/\.\.\/server.js$/);
-          srv.close();
-          done();
-        })
-      });
-    });
-
+    });      
   });
 });
